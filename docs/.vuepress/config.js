d49c132fa14b316db4735b5cac930aa75816491f
13655079137 <731003808@qq.com>
2019-10-31 09:47:13 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,7 +33,7 @@ module.exports = {
     pagination: '5', //每一页显示的文章个数
     avatar: '/logo.jpg', //头像地址
     brand: '/brand.jpg', //头像背景图片地址
-    github: 'https://gitee.com/chen2114/projects', //点击github跳转的地址
+    github: 'https://gitee.com/chen2114', //点击github跳转的地址
     vssue: {
       //评论的配置,
       need: false, //是否需要评论
